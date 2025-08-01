d45dc80a15f43daf4d7f36c8076ef90cf0d489c8
@@123@Beatbacklist <dev1samtechstudiolab@gmail.com>
2025-08-01 03:32:30 +0000

    rem

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,6 @@ export const revalidate = 0
 
 export default async function HomePage() {
   const homedata = await homeData()
-  console.log('Home Page Data:', homedata);
   
   return (
     <HomePageComponent content={homedata} />
