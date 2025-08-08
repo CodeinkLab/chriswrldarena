343fa7603ca6e5f430b98368ddc141e8db159f7c
@@123@Beatbacklist <dev1samtechstudiolab@gmail.com>
2025-08-08 13:56:11 +0000

    sort

--- app/components/shared/TableSeater.tsx
+++ app/components/shared/TableSeater.tsx
@@ -69,8 +69,8 @@ export function TableComponent<T>({
   const [currentPage, setCurrentPage] = useState(1);
 
   // If slice is provided, use it instead of pagination
-  const displayData = slice ? data.slice(0, slice).sort((a, b) => new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime())
-    : data.sort((a, b) => new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime());
+  const displayData = slice ? data.slice(0, slice).sort((a, b) => new Date(a.publishedAt).getTime() - new Date(b.publishedAt).getTime())
+    : data.sort((a, b) => new Date(a.publishedAt).getTime() - new Date(b.publishedAt).getTime());
 
   // Calculate pagination values
   const totalPages = Math.ceil(data.length / pageSize);
